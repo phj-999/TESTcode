3938122d91fc5e65de1e22891f1a2c74f5c3b818
phj-999 <865779043@qq.com>
2021-03-20 09:40:18 +0800

    1
    1t

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,76 @@
-// pages/goods_detail/index.js
+
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsObj: {},
+    // 商品对象
+  GoodsInfo: {},
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    const {goods_id} = options
+    this.getGoodsDetail(goods_id)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getGoodsDetail(goods_id){
+     wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/goods/detail',  
+      data:{
+        goods_id
+      },
+      header: {
+        "content-type": "application/json"
+      },
+      success:(res)=> {
+           console.log(res)
+           this.GoodsInfo = goodsObj;
+          const goodsObj = res.data.message
+         
+           this.setData({
+           goodsObj: {
+            goods_name:  goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
+           }
+           }) 
+          }
+        
+      })
+   },
+   handlePrevewImage(e) {
+    // 1 先构造要预览的图片数组 
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
+    // 2 接收传递过来的图片url
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleCartAdd() {
+    // 1 获取缓存中的购物车 数组
+    let cart = wx.getStorageSync("cart") || [];
+    // 2 判断 商品对象是否存在于购物车数组中
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      //3  不存在 第一次添加
+      this.GoodsInfo.num = 1;
+     // this.GoodsInfo.checked = true;
+      cart.push(this.GoodsInfo);
+    } else {
+      // 4 已经存在购物车数据 执行 num++
+      cart[index].num++;
+    }
+    // 5 把购物车重新添加回缓存中
+    wx.setStorageSync("cart", cart);
+    // 6 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      // true 防止用户 手抖 疯狂点击按钮 
+      mask: true
+    })
   }
 })
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,80 +1,95 @@
-// pages/goods_list/index.js
-Page({
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-tabBar:[
-  {id:0,value:"综合",isActive:true},
-  {id:1,value:"销量",isActive:false},
-  {id:2,value:"价格",isActive:false}
-]
+    
+    tabs: [
+      {
+        id: 0,
+        value: "综合",
+        
+      },
+      {
+        id: 1,
+        value: "销量",
+        
+      },
+      {
+        id: 2,
+        value: "价格",
+      
+      }
+    ],
+    active:0,
+    goodsList:[]
   },
-
+  parmas:[{query:"",
+    cid:"",
+    pagenum:1,
+    pagesize:10}],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getgoodslist()  
   },
+  getgoodslist(){
+    var  that = this
+    wx.showLoading({
+      title: "加载中",
+      mask: true
+    }),
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/goods/search',  
+      data:{
+        query:"",
+    cid:"",
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-  handleTabsItemChange(e){
-    //获取点击索引
-    const{index}=e.detail
-    //修改原数组
-    let{tabBar}=this.data
-    tabBar.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false)
-    //赋值渲染
-    this.setData({
-      tabBar:tabBar
+    pagenum:"",
+    pagesize:10
+      },
+      success(res) {
+       // var that =this
+       // console.log(res.data)
+        let code = res.statusCode.toString()
+        if (code.startsWith('2')) {
+          wx.hideLoading()
+           console.log(res.data)
+           const goodsList = res.data.message
+           that.setData({
+             //appData中图片和数据在goods中  要解构出来
+            goodsList:[...goodsList,...goodsList.goods]
+           })
+          }else{
+            wx.hideLoading()
+            wx.showToast({
+            title: '连接失败，稍后再试',
+            icon: 'error',
+            mask:true,
+            duration: 2000
+          })}
+        
+      },
     })
   },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
+     /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+     this.getgoodslist()
+     this.parmas.pagenum=1
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+      
+  
 
-  }
 })
